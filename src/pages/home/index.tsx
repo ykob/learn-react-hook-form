1efcbfb54cb2cd4d4ef402245fa62ce63b9632df
ykob <info@tplh.net>
2022-12-15 23:54:32 +0900

    importted react-hook-form on the home page.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,37 @@
-import { InputField, InputFieldMulti } from '../../components/ui-parts'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { InputField, ButtonFilled } from '../../components/ui-parts'
+
+type Inputs = {
+  example: string
+  exampleRequired: string
+}
 
 export const PageHome = function () {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  console.log(watch('example'))
+
   return (
-    <div>
-      <InputField placeholder="input text" />
-      <InputField type="number" defaultValue="0" />
-      <InputFieldMulti></InputFieldMulti>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <InputField placeholder="example" {...register('example')} />
+      </div>
+      <div>
+        <InputField
+          placeholder="example required"
+          {...register('exampleRequired', { required: true })}
+        />
+      </div>
+      {errors.exampleRequired && <span>This field is required</span>}
+      <div>
+        <ButtonFilled>Submit</ButtonFilled>
+      </div>
+    </form>
   )
 }
