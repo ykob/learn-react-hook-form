e919a8e98f4a4da5ff1d9eff2fe6acb42a9cffa0
ykob <info@tplh.net>
2022-12-16 22:52:20 +0900

    formatted.

--- src/components/ui-parts/input-field-multi/index.tsx
+++ src/components/ui-parts/input-field-multi/index.tsx
@@ -4,5 +4,9 @@ export const InputFieldMulti = function ({
   children,
   ...props
 }: TextareaHTMLAttributes<HTMLTextAreaElement>) {
-  return <textarea className="p-2 border border-black rounded" {...props}>{children}</textarea>
+  return (
+    <textarea className="p-2 border border-black rounded" {...props}>
+      {children}
+    </textarea>
+  )
 }
--- src/components/ui-parts/input-field/index.tsx
+++ src/components/ui-parts/input-field/index.tsx
@@ -4,5 +4,11 @@ export const InputField = function ({
   type = 'text',
   ...props
 }: InputHTMLAttributes<HTMLInputElement>) {
-  return <input className="px-2 h-8 border border-black rounded" type={type} {...props} />
+  return (
+    <input
+      className="px-2 h-8 border border-black rounded"
+      type={type}
+      {...props}
+    />
+  )
 }
