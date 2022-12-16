346b1e8dd73481666b8114e040be8121b53df82a
ykob <info@tplh.net>
2022-12-16 23:01:11 +0900

    added forwardRef on InputField component.

--- src/components/ui-parts/input-field/index.tsx
+++ src/components/ui-parts/input-field/index.tsx
@@ -1,14 +1,18 @@
-import { InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes } from 'react'
 
-export const InputField = function ({
-  type = 'text',
-  ...props
-}: InputHTMLAttributes<HTMLInputElement>) {
+export const InputField = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>(function (
+  { type = 'text', ...props }: InputHTMLAttributes<HTMLInputElement>,
+  ref
+) {
   return (
     <input
       className="px-2 h-8 border border-black rounded"
+      ref={ref}
       type={type}
       {...props}
     />
   )
-}
+})
