95bf0900bfd4662b3fe79fffdb2b3f90b2468184
ykob <info@tplh.net>
2022-12-20 23:41:15 +0900

    added zodResolver and input number.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MouseEventHandler } from 'react'
 import { Controller, useForm, SubmitHandler } from 'react-hook-form'
 import * as z from 'zod'
@@ -10,7 +11,10 @@ type Inputs = {
 
 const schema = z.object({
   sampleText: z.string().min(1, { message: 'Required' }),
-  sampleNumber: z.number().min(10),
+  sampleNumber: z
+    .number()
+    .min(1, { message: 'Input a number greater than equal to 1.' })
+    .max(10, { message: 'Input a number less than equal to 10.' }),
 })
 
 export const PageHome = function () {
@@ -20,7 +24,8 @@ export const PageHome = function () {
     handleSubmit,
     reset,
   } = useForm<Inputs>({
-    defaultValues: { sampleText: '', sampleNumber: 0 },
+    defaultValues: { sampleText: '', sampleNumber: 1 },
+    resolver: zodResolver(schema),
   })
 
   const onReset: MouseEventHandler<HTMLButtonElement> = () => {
@@ -35,19 +40,25 @@ export const PageHome = function () {
           name="sampleText"
           control={control}
           render={({ field }) => (
-            <InputField placeholder="sampleText" {...field} />
+            <InputField placeholder="sample text" {...field} />
           )}
         />
+        {errors.sampleText?.message && <p>{errors.sampleText?.message}</p>}
       </div>
       <div>
         <Controller
           name="sampleNumber"
           control={control}
-          rules={{ required: true }}
           render={({ field }) => (
-            <InputField placeholder="example required" {...field} />
+            <InputField
+              placeholder="sample number"
+              type="number"
+              {...field}
+              onChange={(e) => field.onChange(parseInt(e.target.value))}
+            />
           )}
         />
+        {errors.sampleNumber?.message && <p>{errors.sampleNumber?.message}</p>}
       </div>
       <div className="flex gap-4">
         <ButtonFilled type="button" onClick={onReset}>
