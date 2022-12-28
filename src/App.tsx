bb7d4032e0b3a2bfd1fa686a7e72d585233b3f04
ykob <info@tplh.net>
2022-12-28 23:07:51 +0900

    added the main heading style.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { PageHome } from './pages'
 function App() {
   return (
     <div className="container mx-auto my-8 px-4 md:px-16">
-      <h1 className="mb-4 text-3xl">Learn React Hook Form</h1>
+      <h1 className="mb-8 text-3xl">Learn React Hook Form</h1>
       <PageHome />
     </div>
   )
