ecb13628c5947e425949d78c57811d128aec5352
ykob <info@tplh.net>
2023-01-11 21:54:07 +0900

    run lint.

--- src/pages/home/components/error-message-block/index.tsx
+++ src/pages/home/components/error-message-block/index.tsx
@@ -11,14 +11,14 @@ export const ErrorMessageBlock = function ({ control }: Props) {
 
   const ErrorMessages = function () {
     const items = Object.keys(errors).map((o) => (
-      <div className='flex flex-wrap gap-x-2'>
-        <div className='font-bold shrink-0'>{o}</div>
+      <div className="flex flex-wrap gap-x-2">
+        <div className="font-bold shrink-0">{o}</div>
         <div>:</div>
         <ErrorMessage errors={errors} name={o} />
       </div>
     ))
 
-    return <div className='flex flex-col gap-2'>{items}</div>
+    return <div className="flex flex-col gap-2">{items}</div>
   }
 
   return (
