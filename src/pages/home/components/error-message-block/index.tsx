748f72327520f3f41e026d91bed97a27b7e3c0a0
ykob <info@tplh.net>
2023-01-11 21:53:21 +0900

    render the all error messages with Object.keys.

--- src/pages/home/components/error-message-block/index.tsx
+++ src/pages/home/components/error-message-block/index.tsx
@@ -9,13 +9,22 @@ type Props = {
 export const ErrorMessageBlock = function ({ control }: Props) {
   const { errors } = useFormState({ control })
 
+  const ErrorMessages = function () {
+    const items = Object.keys(errors).map((o) => (
+      <div className='flex flex-wrap gap-x-2'>
+        <div className='font-bold shrink-0'>{o}</div>
+        <div>:</div>
+        <ErrorMessage errors={errors} name={o} />
+      </div>
+    ))
+
+    return <div className='flex flex-col gap-2'>{items}</div>
+  }
+
   return (
     <div>
-      <h2 className="text-xl">Errors</h2>
-      <ErrorMessage
-        errors={errors}
-        name="sampleText"
-      />
+      <h2 className="mb-2 text-xl">Errors</h2>
+      <ErrorMessages />
     </div>
   )
 }
