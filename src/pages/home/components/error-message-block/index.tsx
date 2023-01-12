8e21c17c70fa6798947352d229c7da9e3d1b61d2
ykob <info@tplh.net>
2023-01-12 22:35:36 +0900

    adjusted styles of the error message block for home.

--- src/pages/home/components/error-message-block/index.tsx
+++ src/pages/home/components/error-message-block/index.tsx
@@ -1,6 +1,7 @@
 import { Control, useFormState } from 'react-hook-form'
 import { ErrorMessage } from '@hookform/error-message'
 import { Inputs } from '../..'
+import { ErrorText } from '../../../../components/ui-parts/'
 
 type Props = {
   control: Control<Inputs>
@@ -11,11 +12,14 @@ export const ErrorMessageBlock = function ({ control }: Props) {
 
   const ErrorMessages = function () {
     const items = Object.keys(errors).map((o) => (
-      <div className="flex flex-wrap gap-x-2">
+      <ErrorText
+        className="flex flex-wrap gap-x-2"
+        key={`error-message-block-${o}`}
+      >
         <div className="font-bold shrink-0">{o}</div>
         <div>:</div>
         <ErrorMessage errors={errors} name={o} />
-      </div>
+      </ErrorText>
     ))
 
     return <div className="flex flex-col gap-2">{items}</div>
