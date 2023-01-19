07dcc8c27345c6b3413cc07bb1d18492b715b485
ykob <info@tplh.net>
2023-01-20 00:04:17 +0900

    Revert "added CheckNumberItems component."
    This reverts commit a63cb84ce34db39b7d919e8fb0132a3ec60a0a93.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,6 @@ import { FormProvider, useForm, SubmitHandler } from 'react-hook-form'
 import * as z from 'zod'
 import {
   CheckItems,
-  CheckNumberItems,
   ErrorMessageBlock,
   RadioItems,
   ViewFormState,
@@ -16,7 +15,6 @@ export type Inputs = {
   textRequired: string
   textAsNumber: number
   textArray: string[]
-  textAsNumberArray: number[]
   textSelectOnlyOne: string
 }
 
@@ -27,7 +25,6 @@ const schema = z.object({
     .min(1, { message: 'Input a number greater than equal to 1.' })
     .max(10, { message: 'Input a number less than equal to 10.' }),
   textArray: z.string().array(),
-  textAsNumberArray: z.string().array(),
   textSelectOnlyOne: z.string(),
 })
 
@@ -37,7 +34,6 @@ export const PageHome = function () {
       textRequired: '',
       textAsNumber: 1,
       textArray: ['Check 1'],
-      textAsNumberArray: [1],
       textSelectOnlyOne: 'Radio 1',
     },
     resolver: zodResolver(schema),
@@ -84,12 +80,6 @@ export const PageHome = function () {
             <CheckItems />
             {errors.textArray?.message && (
               <ErrorText>{errors.textArray?.message}</ErrorText>
-            )}
-          </div>
-          <div className="flex gap-4">
-            <CheckNumberItems />
-            {errors.textAsNumberArray?.message && (
-              <ErrorText>{errors.textAsNumberArray?.message}</ErrorText>
             )}
           </div>
           <div className="flex gap-4">
