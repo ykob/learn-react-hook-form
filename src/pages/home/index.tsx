48cdc39efeecd76c06671bd3daa8bf86d06a3e1d
ykob <info@tplh.net>
2023-01-22 23:05:19 +0900

    splitted schema object.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { MouseEventHandler, useState } from 'react'
 import { FormProvider, useForm, SubmitHandler } from 'react-hook-form'
-import * as z from 'zod'
 import {
   CheckItems,
   ErrorMessageBlock,
@@ -9,6 +8,7 @@ import {
   ViewFormState,
   ViewSubmittedData,
 } from './components/'
+import { schema } from './schema'
 import {
   ButtonFilled,
   Checkbox,
@@ -25,22 +25,6 @@ export type Inputs = {
   textSelectOnlyOne: string
   booleanWithCheck: boolean
 }
-
-const schema = z.object({
-  textRequired: z.string().min(1, { message: 'Required' }),
-  textAsNumber: z
-    .number()
-    .min(1, { message: 'Input a number greater than equal to 1.' })
-    .max(10, { message: 'Input a number less than equal to 10.' }),
-  textAsEmail: z.string().email(),
-  textAsDate: z
-    .date()
-    .min(new Date('1990-01-01'), { message: 'Too old.' })
-    .max(new Date('2100-01-01'), { message: 'Too young.' }),
-  textArray: z.string().array(),
-  textSelectOnlyOne: z.string(),
-  booleanWithCheck: z.boolean(),
-})
 
 export const PageHome = function () {
   const methods = useForm<Inputs>({
---
+++ src/pages/home/schema.ts
@@ -0,0 +1,17 @@
+import * as z from 'zod'
+
+export const schema = z.object({
+  textRequired: z.string().min(1, { message: 'Required' }),
+  textAsNumber: z
+    .number()
+    .min(1, { message: 'Input a number greater than equal to 1.' })
+    .max(10, { message: 'Input a number less than equal to 10.' }),
+  textAsEmail: z.string().email(),
+  textAsDate: z
+    .date()
+    .min(new Date('1990-01-01'), { message: 'Too old.' })
+    .max(new Date('2100-01-01'), { message: 'Too young.' }),
+  textArray: z.string().array(),
+  textSelectOnlyOne: z.string(),
+  booleanWithCheck: z.boolean(),
+})
