577c45221f3dc1d7562a55d6da001adb500f31a8
ykob <info@tplh.net>
2023-01-25 23:12:20 +0900

    adjusted the way to set items on SelectTextEnum component.

--- src/pages/home/components/select-text-enum/index.tsx
+++ src/pages/home/components/select-text-enum/index.tsx
@@ -5,12 +5,38 @@ import { SelectBox } from '../../../../components/ui-parts'
 
 export const SelectTextEnum = memo(function () {
   const { register } = useFormContext<Inputs>()
+  const data = [
+    {
+      id: 'select-text-enum-item-001',
+      label: 'Select 1',
+      value: 'Select 1',
+    },
+    {
+      id: 'select-text-enum-item-002',
+      label: 'Select 2',
+      value: 'Select 2',
+    },
+    {
+      id: 'select-text-enum-item-003',
+      label: 'Select 3',
+      value: 'Select 3',
+    },
+  ]
+  const items = data.map((o) => (
+    <option
+      id={o.id}
+      key={o.id}
+      value={o.value}
+      {...register('textArray')}
+    >
+      {o.label}
+    </option>
+  ))
+
   return (
     <SelectBox {...register('textEnum')}>
       <option value="">Select</option>
-      <option value="1">Select 1</option>
-      <option value="2">Select 2</option>
-      <option value="3">Select 3</option>
+      {items}
     </SelectBox>
   )
 })
